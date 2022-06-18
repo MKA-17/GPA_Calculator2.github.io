5ab81892b875a30efa680470c501909095b3940d
Muhammad Khizar Aslam <2020ee30@student.uet.edu.pk>
2022-06-19 02:43:35 +0500

    GPA Calculator2

---
+++ testergp.js
@@ -0,0 +1,113 @@
+let inp = document.getElementsByClassName("grade");
+let credit = [2, 3, 3, 3, 3, 1, 1, 1, 1]
+let cd = document.getElementsByClassName("Credit");
+//console.log(cd[0].value);
+function calculate() {
+    let i = 0;
+    sum = 0;
+    let gpa = 0;
+    let ch = 0;
+    for (i = 0; i < inp.length; i++) {
+        if(inp[i].value === "WF" || inp[i].value === "None")
+        {continue;}
+        
+        console.log(inp[i].value)
+        sum +=( (parseInt(cd[i].value) * (gradepoint(inp[i].value))));
+        ch += parseInt(cd[i].value);
+        console.log("meow", parseInt(cd[i].value));
+        //console.log( gradepoint(inp[i].value));
+
+    }
+    gpa = sum / ch;
+    console.log(ch);
+    console.log(gpa.toFixed(5));
+    document.getElementById("result").innerHTML = gpa.toFixed(5);
+
+}
+function ADDMORE()
+{
+    //let tabl = document.getElementsByClassName("tab");
+    //let nw = document.createElement;
+    document.getElementById("tabl").innerHTML+="<tr style='padding:10px 20px 10px 20 px;'>"+"<td><input type='text' class='sub' placeholder='SUBJECT'></td>"+"<td><input type='text' class='Credit' placeholder='cd'><td><select class='grade' >"
+                    
+                    +"<option value='None'>None</option>"
+					+"<option value='A+'>A+"
+					+"<option value='A'>A"
+					+"<option value='A-'>A-"
+					+"<option value='B+'>B+"
+					+"<option value='B'>B"
+					+"<option value='B-'>B-"
+					+"<option value='C+'>C+"
+					+"<option value='C'>C"
+					+"<option value='C-'>C-"
+					+"<option value='D+'>D+"
+					+"<option value='D'>D"
+					+"<option value='F'>F"
+                    +"<option value='WF'>WF"
+                    
+				+"</select></tr>"
+                +"<tr>"+"<td><input type='text' class='sub' placeholder='SUBJECT'></td>"+"<td><input type='text' class='Credit' placeholder='cd'><td><select class='grade' >"
+                +"<option value='None'>None</option>"
+					+"<option value='A+'>A+"
+					+"<option value='A'>A"
+					+"<option value='A-'>A-"
+					+"<option value='B+'>B+"
+					+"<option value='B'>B"
+					+"<option value='B-'>B-"
+					+"<option value='C+'>C+"
+					+"<option value='C'>C"
+					+"<option value='C-'>C-"
+					+"<option value='D+'>D+"
+					+"<option value='D'>D"
+					+"<option value='F'>F"
+                    +"<option value='WF'>WF"
+                
+            +"</select></tr>";
+            		
+
+}
+
+function gradepoint(grd)
+ {
+    if (grd == "A+") {
+        return 4;
+    }
+    else if (grd == "A") {
+        return 4;
+    }
+    else if (grd == "A-") {
+        return 3.7;
+    }
+    else if (grd == "B+") {
+        return 3.3;
+    }
+    else if (grd == "B") {
+        return 3;
+    }
+    else if (grd == "B-") {
+        return 2.7;
+    }
+    else if (grd == "C+") {
+        return 2.3;
+    }
+    else if (grd == "C") {
+        return 2;
+    }
+    else if (grd == "C-") {
+        return 1.7;
+    }
+    else if (grd == "D+") {
+        return 1.3;
+    }
+    else if (grd == "D") {
+        return 1;
+    }
+    else if (grd == "F") {
+        return 0;
+    }
+    else {
+        return 0;
+    }
+
+}
+
